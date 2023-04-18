4e1f75009027367fce60babe3b49a19a12e275a6
DaniellePashayan <87950814+DaniellePashayan@users.noreply.github.com>
2023-04-18 11:28:15 -0400

    corrected batch script

--- batch_script.bat
+++ batch_script.bat
@@ -1,10 +1,9 @@
 @Echo Starting Carisk GOA
 
-setlocal
-set "FILE_PATH=%~dp0.."
-set "SCRIPT_PATH=%FILE_PATH%\main.py"
+SETLOCAL
+set FILE_PATH=%~dp0
+set SCRIPT_PATH=%FILE_PATH%main.py
 python -u "%SCRIPT_PATH%"
-endlocal
+ENDLOCAL
 
-python -u "%SCRIPT_PATH%"
 @Echo Process Completed

2aa431b2ec668efaa63786c713d1a33dafffe4f1
danielle pashayan <daniellepashayan@gmail.com>
2024-05-29 09:42:29 -0400

    add venv activation to bat

--- batch_script.bat
+++ batch_script.bat
@@ -1,10 +1,14 @@
-@Echo off
-echo Starting Carisk GOA
+@echo off
+echo Carisk Combine Started
 
 SETLOCAL
 set FILE_PATH=%~dp0
 set SCRIPT_PATH=%FILE_PATH%main.py
+set VENV_PATH=%FILE_PATH%.venv
+
+call "%VENV_PATH%\Scripts\activate.bat"
 python -u "%SCRIPT_PATH%"
 ENDLOCAL
 
-echo Process Completed
+Echo Process Completed
+pause

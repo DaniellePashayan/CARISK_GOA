b863eedfe09907d505f37dfb49044d88d2e09160
danielle pashayan <daniellepashayan@gmail.com>
2024-07-15 09:39:38 -0400

    added logging

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import pandas as pd
 import time
 import shutil
 from glob import glob
+from loguru import logger
 
 
 def get_folder(folder_path) -> tuple[str,str]:
     today = datetime.today()
     delta = timedelta(days=-1)
+    logger.info(f'Getting folder for {folder_path}')
 
-    #
     while True:
         date = today + delta
         if date.weekday() < 5:
@@ -31,6 +32,7 @@ def get_folder(folder_path) -> tuple[str,str]:
 
     dated_path = f'{folder_path}/{year}/{month} {year}/{date_frmt}/'
     date = datetime.strftime(date, '%m%d%Y')
+    logger.info(f'Folder: {dated_path}')
 
     # returns:
     # M:\CPP-Data\Sutherland RPA\MedicalRecords\OC WCNF Records\2023\10 2023\10_27_23
@@ -55,12 +57,12 @@ def has_screenshots(folder_path: str) -> bool:
         for file in file_list:
             if file.endswith('.png'):
                 return True
-                break
             else:
                 return False
 
 
 def monitor_folder(folder_path: str, date_frmt: str, interval=60) -> None:
+    logger.info(f'Monitoring {folder_path} for new files')
     prev_file_count = get_file_count(folder_path)
     if prev_file_count > 0:
         keep_checking = True
@@ -73,15 +75,16 @@ def monitor_folder(folder_path: str, date_frmt: str, interval=60) -> None:
         curr_file_count = get_file_count(folder_path)
 
         if curr_file_count > prev_file_count:
-            print("New files detected")
+            logger.warning("New files detected")
             prev_file_count = curr_file_count
         else:
-            print("No new files detected after 60 seconds")
+            logger.success("No new files detected after 60 seconds")
             run(folder_path, date_frmt)
             keep_checking = False
 
 
 def move_error_screenshots(dated_path):
+    logger.info(f'Moving error screenshots to error folder')
     error_path = f'{dated_path}/error screenshots'
 
     if os.path.exists(dated_path):
@@ -90,15 +93,19 @@ def move_error_screenshots(dated_path):
         for filename in os.listdir(dated_path):
             if filename.endswith('.png'):
                 shutil.move(f'{dated_path}/{filename}', error_path)
+    logger.success(f'Error screenshots moved to {error_path}')
 
 
 def run(dated_path: str, date_frmt: str) -> None:
+    os.makedirs('./logs', exist_ok=True)
+    logger.add(f'./logs/log_{time}.log', rotation='1 day', retention='7 days', level='INFO')
     dest_path = 'M:/CPP-Data/Sutherland RPA/MedicalRecords/OC WCNF Records/GOA/'
     log_path = 'M:/CPP-Data/Sutherland RPA/MedicalRecords/OC WCNF Records/script logs/'
 
-    date = pd.to_datetime(date_frmt, format="%m_%d_%y").strftime('%m%d%Y')
+    # date = pd.to_datetime(date_frmt, format="%m_%d_%y").strftime('%m%d%Y')
+    logger.info(f'Processing {date_frmt} folder')
 
-    print(dated_path)
+    logger.debug(dated_path)
     if os.path.exists(dated_path):
         # df = read_input_file(date)
         # df = df['INVNUM'].astype(str).tolist()
@@ -148,6 +155,7 @@ def run(dated_path: str, date_frmt: str) -> None:
 
     else:
         print("Folder missing")
+        logger.warning(f'{dated_path} is missing')
 
 
 def read_input_file(date: str) -> pd.DataFrame:

58bacca4aff1dc07fd3a098018c93da80a34b356
danielle pashayan <daniellepashayan@gmail.com>
2024-07-15 09:39:29 -0400

    changed .bat for server

--- batch_script.bat
+++ batch_script.bat
@@ -2,13 +2,13 @@
 echo Carisk Combine Started
 
 SETLOCAL
-set FILE_PATH=%~dp0
-set SCRIPT_PATH=%FILE_PATH%main.py
-set VENV_PATH=%FILE_PATH%.venv
-
+set FILE_PATH="C:\Users\pa_dpashayan\Desktop\PyProjects\CARISK_GOA"
+cd %FILE_PATH%
+set VENV_PATH=%FILE_PATH\%.venv
 call "%VENV_PATH%\Scripts\activate.bat"
+set SCRIPT_PATH=%FILE_PATH\%main.py
+
 python -u "%SCRIPT_PATH%"
 ENDLOCAL
 
 Echo Process Completed
-pause
